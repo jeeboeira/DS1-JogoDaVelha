4defbc0d9fa0dc852b2555c476ac17c7da9b5723
Jessé Boeira <jee_boeira@hotmail.com>
2024-12-12 13:19:00 -0300

    Me perdi

--- src/main/java/com/jesseboeira/jogodavelha/Screens/SplashScreen.java
+++ src/main/java/com/jesseboeira/jogodavelha/Screens/SplashScreen.java
@@ -2,7 +2,6 @@ package com.jesseboeira.jogodavelha.Screens;
 
 import com.jesseboeira.jogodavelha.utils.ScreenUtils;
 import javafx.scene.layout.StackPane;
-import javafx.scene.input.KeyEvent;
 
 public class SplashScreen {
     private final ScreenManager screenManager;
@@ -21,12 +20,5 @@ public class SplashScreen {
         // Configura os eventos
         screenManager.getCurrentScene().setOnKeyPressed(this::handleKeyPress);
         screenManager.getCurrentScene().setOnMouseClicked(this::handleKeyPress);
-    }
-    private void handleKeyPress(KeyEvent event) {
-        switch (event.getCode()) {
-            case ENTER -> System.out.println("Enter pressed! Starting the game...");
-            case ESCAPE -> System.out.println("Escape pressed! Exiting...");
-            default -> System.out.println("Key pressed: " + event.getCode());
-        }
     }
 }
--- src/main/java/com/jesseboeira/jogodavelha/controller/SplashScreenController.java
+++ src/main/java/com/jesseboeira/jogodavelha/controller/SplashScreenController.java
@@ -1,12 +1,20 @@
 package com.jesseboeira.jogodavelha.controller;
 
+import com.jesseboeira.jogodavelha.Screens.ScreenManager;
 import javafx.scene.input.KeyEvent;
 
 public class SplashScreenController {
-    private void handleKeyPress(KeyEvent event) {
+    private final ScreenManager screenManager;
+
+    public SplashScreenController(ScreenManager screenManager) {
+        this.screenManager = screenManager;
+    }
+
+    private void handleSplashEvent() {
         switch (event.getCode()) {
             case ENTER -> System.out.println("Enter pressed! Starting the game...");
             case ESCAPE -> System.out.println("Escape pressed! Exiting...");
             default -> System.out.println("Key pressed: " + event.getCode());
         }
+    }
 }
